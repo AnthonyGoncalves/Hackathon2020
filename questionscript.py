278fa6fb3a95900dcf99aee59d09e71fe8b2cf86
Rich Imbro <rimbro1@binghamton.edu>
2020-02-08 14:30:44 -0500

    added script

---
+++ questionscript.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class UserInput():
+
+requests.get("")
+
+    def userinput():
+        sports = input("Do you like sports?")
+        country = input("Do you like  country music?")
+        rap = input("Do  you like rap music?")
+
+
+    def sports():
+        if ____ is True:
+            sports = True
+        else:
+            sports = False
+
+
+music =
+
+
+main()
