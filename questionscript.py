7f9feaa1cc9f759b57d7c777a954d927279b3ea3
Rich Imbro <rimbro1@binghamton.edu>
2020-02-08 15:06:02 -0500

    template

--- questionscript.py
+++ questionscript.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello! this is the main page <h1> HELLO <h1>"
+    print(input("Can you answer this?"))
 
 @app.route("/<name>")
 def user(name):
@@ -13,7 +14,7 @@ def user(name):
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("home"))
+    return redirect(url_for("user", name="Admin!"))
 
 if  __name__ == "__main__":
     app.run()
