e05236e4c63de40b073e66aed21bb4e948a0b597
Rich Imbro <rimbro1@binghamton.edu>
2020-02-08 16:44:02 -0500

    i am so sorry

--- questionscript.py
+++ questionscript.py
@@ -3,9 +3,13 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("StartPage.HTML")
+
+@app.route("/filloutform")
+def form():
+    return render_template("FormPage.HTML")
 
 if  __name__ == "__main__":
     app.run()
