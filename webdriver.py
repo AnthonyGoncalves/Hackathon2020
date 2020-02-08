3c775d8290faa5a266b15718d720ef08d96adf43
Tom Fasano <54379134+tfasano1@users.noreply.github.com>
2020-02-08 16:41:55 -0500

    touching up webdriver.py

--- webdriver.py
+++ webdriver.py
@@ -10,20 +10,23 @@ class GroupMeBot:
         self.driver = webdriver.Chrome()
 
     def logIn(self):
-        # Go to GroupMe.com!
-        self.driver.get('https://groupme.com')
-        sleep(1)
+        try:
+            # Go to GroupMe.com!
+            self.driver.get('https://groupme.com')
+            sleep(1)
 
-        # Clicks Log in
-        self.driver.find_element_by_xpath('/html/body/div[2]/div/div/p/a').click()
-        sleep(1)
+            # Clicks Log in
+            self.driver.find_element_by_xpath('/html/body/div[2]/div/div/p/a').click()
+            sleep(1)
 
-        #Enters email and password, then clicks Log in!
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div/form/div[1]/input").send_keys(self.email)
-        self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/form/div[2]/input').send_keys(self.password)
-        self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/form/button').click()
+            #Enters email and password, then clicks Log in!
+            self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div/form/div[1]/input").send_keys(self.email)
+            self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/form/div[2]/input').send_keys(self.password)
+            self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/form/button').click()
 
-        sleep(100)
+            sleep(100)
+        except Exception as e:
+            print('[EXCEPTION]', e)
 
     def addClient(self):
         pass
