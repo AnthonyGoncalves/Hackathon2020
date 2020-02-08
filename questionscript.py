eea8180767bdc3463bbc26fbce6cc874b9586c31
Rich Imbro <rimbro1@binghamton.edu>
2020-02-08 15:53:02 -0500

    now its working

--- questionscript.py
+++ questionscript.py
@@ -1,20 +1,11 @@
 import requests
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! this is the main page <h1> HELLO <h1>"
-    print(input("Can you answer this?"))
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin!"))
+    return render_template("index.html")
 
 if  __name__ == "__main__":
     app.run()
