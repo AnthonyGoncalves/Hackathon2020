67a1c7beb96e7a492a5f14f4de8f0bf4cd2df5c6
Tom Fasano <54379134+tfasano1@users.noreply.github.com>
2020-02-08 22:18:25 -0500

    Fully functioning Bot!

--- webdriver.py
+++ webdriver.py
@@ -1,14 +1,28 @@
 from selenium import webdriver
 from time import sleep
+from pynput.keyboard import Key, Controller
 import questionscript
+#import client_data
+
+
+
 
 class GroupMeBot:
 
-    def __init__(self):
-
+    def __init__(self ):
+        # Bot info
         self.email = 'hackbubot@gmail.com'
         self.password = 'password'
         self.driver = webdriver.Chrome()
+
+        # Client info
+        self.name = 'Anthony'
+        self.phone_number = '9148605694'
+        self.pref = 'Video Games'
+
+        # Bot Input
+        self.keyboard = Controller()
+
 
     def logIn(self):
         try:
@@ -25,15 +39,56 @@ class GroupMeBot:
             self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/form/div[2]/input').send_keys(self.password)
             self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/form/button').click()
 
-            sleep(100)
+            sleep(1)
+
+
         except Exception as e:
             print('[EXCEPTION]', e)
 
-    def addClient(self):
-        pass
+    def addClientToGroup(self):
 
-    def addToGroup(self):
-        pass
+        # Click Search Bar for pref
+        self.driver.find_element_by_xpath('/html/body/div[1]/aside/div[2]/div[2]/input').send_keys(self.pref)
+        sleep(0.2)
+        for i in range(2):
+            self.keyboard.press('\t')
+            self.keyboard.release('\t')
+        sleep(1)
+        self.keyboard.press(Key.enter)
+        self.keyboard.release(Key.enter)
+        sleep(1)
+        for i in range(17):
+            self.keyboard.press('\t')
+            self.keyboard.release('\t')
+            sleep(.2)
+        for i in range(2):
+            self.keyboard.press(Key.enter)
+            self.keyboard.release(Key.enter)
+        for i in range(4):
+            self.keyboard.press('\t')
+            self.keyboard.release('\t')
+            sleep(.5)
+        self.keyboard.press(Key.enter)
+        self.keyboard.release(Key.enter)
+        sleep(1)
+        self.keyboard.type(self.phone_number)
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div[3]/div[2]/button').click()
+        sleep(.5)
+        self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div[1]/div[1]/input').send_keys(self.name)
+        '''for i in range(2):
+            self.keyboard.press('\t')
+            self.keyboard.release('\t')
+            sleep(.2)'''
+        # Add
+        sleep(1)
+        self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div[2]/button').click()
+        sleep(1)
+        # Add 1 member
+        self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/button').click()
+
+        sleep(.5)
 
 my_bot = GroupMeBot()
 my_bot.logIn()
+my_bot.addClientToGroup()
