26e1a8e4ff02fbc4f6b0ecf5314bccbfb1768fa8
Anthony Goncalves <agoncal2@binghamton.edu>
2020-02-09 07:25:47 -0500

    fixed

--- questionscript.py
+++ questionscript.py
@@ -1,24 +1,38 @@
 import requests
+import json
 from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("StartPage.HTML")
+	return render_template("StartPage.HTML")
 
-@app.route("/filloutform")
+@app.route("/filloutform", methods=['POST', 'GET'])
 def form():
-    return render_template("FormPage.HTML")
+	if request.method == 'POST':
+		sports = request.form.get('front')
+		fashion = request.form.get('name')
+		movie = request.form.get('movie')
+		food = request.form.get('find')
+		music = request.form.get('keeping')
+		vg = request.form.get('ambiance')
+		lang = request.form.get('lang')
+		code = request.form.get('code')
+		user = request.form.get('fname')
+		return redirect(url_for("user",usr = user))
+	else:
+		return render_template("FormPage.HTML")
 
-@app.route("/info", methods=['POST', 'GET'])
-def jackpot():
-    return render_template("info.json")
+@app.route("/<usr>")
+def user(usr):
+	return "<h1>{usr}</h1>"
 
-@app.route("/Popular")
-def home():
-    return render_template("PopularPage.HTML")
+#@app.route("/info", methods=['POST', 'GET'])
+#def jackpot():
+	#return render_template("info.json")
+
 
 
 if  __name__ == "__main__":
-    app.run()
+	app.run()
